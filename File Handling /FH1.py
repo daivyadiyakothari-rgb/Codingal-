9963a43cd4962d89af68920c7c124a679db66eb7
daivyadiyakothari-rgb <daivyadiyakothari@gmail.com>
2026-02-23 18:25:56 +0530

    FH1

--- File Handling /FH1.py
+++ File Handling /FH1.py
@@ -21,3 +21,26 @@ file_append = open('Codingal.txt', 'a')
 file_append.write("\nFile in append mode....")
 file_append.write("Hi! I am Penguin. I am 3 year old")
 file_append.close()
+
+# After Class Project
+names = ["Diya", "Daivya", "Divolka", "Diva"]
+
+# Write mode
+with open("students.txt", "w") as file:
+    file.write("Student Introduction List\n")
+    file.write("-------------------------\n")
+
+# Append mode
+with open("students.txt", "a") as file:
+    for i in range(len(names)):
+        file.write(str(i + 1) + ". My name is " + names[i] + "\n")
+
+# Read mode
+with open("students.txt", "r") as file:
+    print(file.read())
+
+# Read and Write mode
+with open("students.txt", "r+") as file:
+    old_content = file.read()
+    file.seek(0)
+    file.write("Class Monitor: Roy\n\n" + old_content)
