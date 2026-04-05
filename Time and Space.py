8638b4e1e119b21dedf15b722ed56c60fa521267
Daivya Kothari <daivyadiyakothari@gmail.com>
2026-04-05 17:48:55 +0530

    Time and Space

--- Time and Space.py
+++ Time and Space.py
@@ -9,3 +9,17 @@ result = fun1(n)
 print("Sum of first", n, "natural number is", result)
 
 # After Class Project
+def multiply_iterative(n,m):
+    result = 0
+    for i in range(m):
+        result += n
+    return result
+
+n = int(input("Enter value of N: "))
+m = int(input("Enter value of M: "))
+
+direct_result = n * m
+iterative_result = multiply_iterative(n,m)
+
+print("\nUsing direct multiplication: ", direct_result)
+print("Using iterative multiplication: ", iterative_result)
