a0c36881450dc3f81d59d500f714dbe3ecef621e
Daivya Kothari <daivyadiyakothari@gmail.com>
2026-04-05 17:43:49 +0530

    Time and Space

---
+++ Time and Space.py
@@ -0,0 +1,11 @@
+# Activity 1
+def fun1(n):
+    return n * (n+1) /2
+print(fun1(100))
+
+# Activity 2
+n = int(input("Enter a number: "))
+result = fun1(n)
+print("Sum of first", n, "natural number is", result)
+
+# After Class Project
